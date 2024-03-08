65542bb8ed81c39b16793cf8c63d0a7419dc737b
bedolaga66 <160242954+bedolaga66@users.noreply.github.com>
2024-03-08 21:39:29 +0500

    Added swapping method

---
+++ Assets/Scripts/BackgroundTIle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initiallize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initiallize()
+    {
+        
+    }
+
+    
+}
