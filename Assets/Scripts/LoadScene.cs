3719b2830d0abc71f3122fba0e488a23d8b1ff0a
PeachBROKN <160254687+PeachBROKN@users.noreply.github.com>
2024-03-16 02:33:56 +0500

    Add many scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public int sceneID;
+
+    public Slider loadSlider;
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+
+    IEnumerator LoadNextScene()
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync(sceneID);
+        while (!oper.isDone) 
+        {
+            float progress = oper.progress / 0.9f;
+            loadSlider.value = progress;
+            yield return null;
+        }
+    }
+}
