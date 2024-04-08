75425ebf58b30791c6dc99ae156364eb730b3ea7
bedolaga66 <160242954+bedolaga66@users.noreply.github.com>
2024-04-09 02:36:53 +0500

    Background for goals panel&score counter improved, score bar removed

--- Assets/Scripts/GameScripts/ScoreManager.cs
+++ Assets/Scripts/GameScripts/ScoreManager.cs
@@ -19,7 +19,7 @@ public class ScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "" + score;
+        scoreText.text = score.ToString() + "/" + UpdateScoreToGoalAmount(score).ToString();
         
     }
 
@@ -27,6 +27,27 @@ public class ScoreManager : MonoBehaviour
     {
         score+=amountToIncrease;
         UpdateBar();
+    }
+
+    public int UpdateScoreToGoalAmount(int score)
+    {
+        int i = 0;
+        if (score < board.scoreGoals[i])
+        {
+            return board.scoreGoals[i];
+        }
+        else if (score >= board.scoreGoals[i] && score < board.scoreGoals[i + 1])
+        {
+            return board.scoreGoals[i + 1];
+        }
+        else if (score >= board.scoreGoals[i + 1] && score < board.scoreGoals[i + 2])
+        {
+            return board.scoreGoals[i + 2];
+        }
+        else
+        {
+            return board.scoreGoals[i + 2];
+        }
     }
 
     public void UpdateBar()
